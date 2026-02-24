2f1f669bc2e2cccb4b30b15568d5af404601167e
kadgitub7 <kadhir.ponnambalam@gmail.com>
2026-02-24 08:27:44 -0500

    fix implicit failures
    I forgot to assign wire to the intermediate steps in A and B, but Verilog auto assigns these values so it wasn't crashing, but bad practice. Also took out S because we were never using it anywhere

--- fourBitAdder.v
+++ fourBitAdder.v
@@ -6,7 +6,7 @@ module fourBitAdder(
     output Co
     );
     
-    wire c0,c1,c2;
+    wire c0,c1,c2,a3,a2,a1,a0,b3,b2,b1,b0;
     
     assign {a3,a2,a1,a0} = A;
     assign {b3,b2,b1,b0} = B;
@@ -15,7 +15,5 @@ module fourBitAdder(
     fullAdder f2(a1,b1,c0,S1,c1);
     fullAdder f3(a2,b2,c1,S2,c2);
     fullAdder f4(a3,b3,c2,S3,Co);
-    
-    assign S = {S3,S2,S1,S0};
     
 endmodule
